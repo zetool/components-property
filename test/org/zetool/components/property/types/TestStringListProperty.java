957db77098e1bac1437a9c13f93dcbfeb6af5523
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2015-09-13 00:30:46 +0200

    Tests for stringlistproperty

--- test/org/zetool/components/property/types/TestStringListProperty.java
+++ test/org/zetool/components/property/types/TestStringListProperty.java
@@ -59,4 +59,53 @@ public class TestStringListProperty {
     public void testFailForWrongType() {
         stringListProperty.setValue("fails");
     }
+    
+    @Test
+    public void testEmptyInitialized() {
+        assertThat(stringListProperty.size(), is(equalTo(0)));
+    }
+    
+    @Test
+    public void testContains() {
+        stringListProperty.add("test value");
+        assertThat(stringListProperty.size(), is(equalTo(1)));
+        assertThat(stringListProperty.contains("test value"), is(true));
+        assertThat(stringListProperty.contains("not contained"), is(false));
+    }
+    
+    @Test
+    public void testRemove() {
+        stringListProperty.add("test value");
+        stringListProperty.add("another value");
+        stringListProperty.remove("test value");
+        assertThat(stringListProperty.size(), is(equalTo(1)));
+        assertThat(stringListProperty.contains("test value"), is(false));
+        assertThat(stringListProperty.contains("another value"), is(true));
+    }
+    
+    @Test
+    public void testAddMultiple() {
+        Collection<String> c = new HashSet<>(count);
+        populate(c);
+        stringListProperty.addAll(c);
+        assertThat(stringListProperty.size(), is(equalTo(count)));
+        for(int i = 1; i <= count; ++i){
+            assertThat(stringListProperty.contains(elementGenerator.apply(i)), is(true));
+        }
+        
+    }
+  
+    @Test
+    public void removeMultiple() {
+        Collection<String> c = new HashSet<>(count);
+        populate(c);
+        stringListProperty.addAll(c);
+        c.remove(elementGenerator.apply(1));
+        stringListProperty.removeAll(c);
+        assertThat(stringListProperty.size(), is(equalTo(1)));
+        assertThat(stringListProperty.contains(elementGenerator.apply(1)), is(true));
+        assertThat(stringListProperty.contains(elementGenerator.apply(2)), is(false));
+        assertThat(stringListProperty.contains(elementGenerator.apply(3)), is(false));
+    }
+            
 }
