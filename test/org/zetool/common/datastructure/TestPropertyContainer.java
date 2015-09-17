3c9853552c52811e5da5fc03180fe3a94c727da6
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2015-09-17 23:19:47 +0200

    Final test cases for property container.

--- test/org/zetool/common/datastructure/TestPropertyContainer.java
+++ test/org/zetool/common/datastructure/TestPropertyContainer.java
@@ -1,8 +1,13 @@
 package org.zetool.common.datastructure;
 
 import ds.PropertyContainer;
+import java.awt.Color;
+import java.util.Arrays;
+import java.util.function.Function;
+import javax.swing.UIManager;
 import org.hamcrest.CoreMatchers;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
@@ -15,68 +20,119 @@ import org.junit.Test;
  * @author Jan-Philipp Kappmeier
  */
 public class TestPropertyContainer {
-    PropertyContainer pc;
+    PropertyContainer propertyContainer;
     
     @Before
     public void initPropertyContainer() {
-        pc = new PropertyContainer();
+        propertyContainer = new PropertyContainer();
     }
     
     @Test
     public void testPropertyContainer() {
-        assertThat(pc.size(), is(equalTo(0)));
-        pc.define("some parameter", Boolean.class, false);
-        assertThat(pc.size(), is(equalTo(1)));
-        pc.define("some other parameter", Boolean.class, true);
-        assertThat(pc.size(), is(equalTo(2)));
-        assertThat(pc.getAs("some parameter", Boolean.class), is(equalTo(false)));
-        assertThat(pc.getAs("some other parameter", Boolean.class), is(equalTo(true)));
+        assertThat(propertyContainer.size(), is(equalTo(0)));
+        propertyContainer.define("some parameter", Boolean.class, false);
+        assertThat(propertyContainer.size(), is(equalTo(1)));
+        propertyContainer.define("some other parameter", Boolean.class, true);
+        assertThat(propertyContainer.size(), is(equalTo(2)));
+        assertThat(propertyContainer.getAs("some parameter", Boolean.class), is(equalTo(false)));
+        assertThat(propertyContainer.getAs("some other parameter", Boolean.class), is(equalTo(true)));
+        assertThat(propertyContainer, contains("some parameter", "some other parameter"));
     }
     
     @Test
     public void testContainment() {
-        assertThat(pc.isDefined("test parameter"), is(false));
-        pc.define("test parameter", Integer.class, 2);
-        assertThat(pc.isDefined("test parameter"), is(true));
+        assertThat(propertyContainer.isDefined("test parameter"), is(false));
+        propertyContainer.define("test parameter", Integer.class, 2);
+        assertThat(propertyContainer.isDefined("test parameter"), is(true));
     }
     
     @Test(expected = IllegalArgumentException.class)
     public void testDoubleDefineFails() {
-        pc.define("some parameter", Integer.class, 1);
-        pc.define("some parameter", Integer.class, 2);
-        
+        propertyContainer.define("some parameter", Integer.class, 1);
+        propertyContainer.define("some parameter", Integer.class, 2);
     }
     
     @Test
     public void typeStoredCorrect() {
-        pc.define("double parameter", Double.class, 1.0);
-        pc.define("float parameter", Float.class, 1.0f);
-        assertThat(pc.getType("double parameter"), is(equalTo(Double.class)));
-        assertThat(pc.getType("float parameter"), is(equalTo(Float.class)));
+        propertyContainer.define("double parameter", Double.class, 1.0);
+        propertyContainer.define("float parameter", Float.class, 1.0f);
+        assertThat(propertyContainer.getType("double parameter"), is(equalTo(Double.class)));
+        assertThat(propertyContainer.getType("float parameter"), is(equalTo(Float.class)));
     }
     
     @Test
     public void testSetting() {
-        pc.define("param", Integer.class, -1);
-        pc.set("param", 1);
-        assertThat(pc.getAs("param", Integer.class), is(equalTo(1)));
+        propertyContainer.define("param", Integer.class, -1);
+        propertyContainer.set("param", 1);
+        assertThat(propertyContainer.getAs("param", Integer.class), is(equalTo(1)));
     }
     
     @Test(expected = IllegalArgumentException.class)
     public void testSettingFailsForUndefined() {
-        pc.set("not defined", 2);
+        propertyContainer.set("not defined", 2);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testSettingFailsWrongType() {
+        propertyContainer.define("intparam", Integer.class, 1);
+        try {
+            propertyContainer.set("intparam", 3.4);
+        } catch (IllegalArgumentException ex ) {
+            assertThat(ex.getMessage(), containsString("java.lang.Integer"));
+            throw ex;
+        }
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetAsNotContained() {
+        propertyContainer.getAs("my key", Integer.class);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetAsBadType() {
+        propertyContainer.define("intparam", Integer.class, 0);
+        try {
+            propertyContainer.getAs("intparam", Double.class);
+        } catch (IllegalArgumentException ex ) {
+            assertThat(ex.getMessage(), containsString("java.lang.Integer"));
+            assertThat(ex.getMessage(), containsString("java.lang.Double"));
+            throw ex;
+        }
     }
     
     @Test
-    public void testSettingFailsWrongType() {
-        pc.define("intparam", Integer.class, 1);
+    public void testGetAsSupertype() {
+        propertyContainer.define("intparam", Integer.class, 2);
+        Number n = propertyContainer.getAs("intparam", Number.class);
+        assertThat(n, is(equalTo(2)));
+    }
+    
+    @Test
+    public void testSimpleGet() {
+        propertyContainer.define("some param", Integer.class, 1);
+        
+    }
+    
+    @Test
+    public void testGetAsSpecific() {
+        assertSpecificProperty(propertyContainer::get, Integer.MAX_VALUE);
+        assertSpecificProperty(propertyContainer::getAsBoolean, true);
+        assertSpecificProperty(propertyContainer::getAsColor, Color.DARK_GRAY);
+        assertSpecificProperty(propertyContainer::getAsDouble, Math.PI);
+        assertSpecificProperty(propertyContainer::getAsFont, UIManager.getFont("TextField.font"));
+        assertSpecificProperty(propertyContainer::getAsString, "some string");
+        assertSpecificProperty(propertyContainer::getAsStringList, Arrays.asList(new String[] {"item 1", "item 2"}));
+    }
+    
+    private <T> void assertSpecificProperty(Function<String,T> specificAccessor, T propertyValue) {
+        propertyContainer.define(propertyValue.getClass().toString(), (Class<T>)propertyValue.getClass(), propertyValue);
+        assertThat(specificAccessor.apply(propertyValue.getClass().toString()), is(equalTo(propertyValue)));
         try {
-            pc.set("intparam", 3.4);
-        } catch (IllegalArgumentException ex ) {
-            assertThat(ex.getMessage(), containsString("java.lang.Integer"));
+            specificAccessor.apply("not contained");
+        } catch (IllegalArgumentException ex) {
             return;
         }
-        throw new AssertionError("Should throw " + IllegalAccessException.class);
+        throw new AssertionError();
     }
     
     @Test
