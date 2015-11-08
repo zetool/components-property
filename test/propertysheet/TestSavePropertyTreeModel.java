e46add715f7cd2db66f37961cfdcef469af9aee4
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2015-11-08 19:51:30 +0100

    crlf

--- test/propertysheet/TestSavePropertyTreeModel.java
+++ test/propertysheet/TestSavePropertyTreeModel.java
@@ -11,7 +11,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 package propertysheet;
 
@@ -157,7 +157,7 @@ public class TestSavePropertyTreeModel {
         d.setVisible(true);
     }
 
-    public static void maina(String[] args) {
+    public static void main(String[] args) {
         PropertyTreeNode root = new PropertyTreeNode("my_root_name");
         PropertyTreeModel ptm = new PropertyTreeModel(root);
         root.setDisplayName("my_displayname");
